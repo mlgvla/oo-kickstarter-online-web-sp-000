b7fc1bb5b203e3111ceee82b1a7bc71bdd50b70a
Monica Gerard <mlgerardvla@gmail.com>
2020-03-23 02:22:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project
 
-  attr_accessor :name
-  attr_reader :backers
+  attr_accessor
+  attr_reader :backers, :title
 
-  def initialize(name)
-    @name = name
+  def initialize(titla)
+    @title = title
     @backers = []
   end
 end
